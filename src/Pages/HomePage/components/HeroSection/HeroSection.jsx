a4fca88475e78665d97f95e660559aba5c22861f
klimisnotavailable <klimgrigor28@gmail.com>
2024-08-31 11:37:13 +0300

    klym/hero-section/feat

--- src/Pages/HomePage/components/HeroSection/HeroSection.jsx
+++ src/Pages/HomePage/components/HeroSection/HeroSection.jsx
@@ -3,49 +3,94 @@ import Icon from "../../../../Components/Icon/Icon";
 import icons from "../../../../images/favicon/icons.svg";
 import heroImage1 from "../../../../images/shared/mobile/hero-image-1.png";
 import heroImage1_2x from "../../../../images/shared/mobile/hero-image-1@2x.png";
-import heroBg from "../../../../images/shared/backgrounds/hero-bg-mobile.png";
+import heroImage1_desktop from "../../../../images/shared/desktop/hero-image-1-desktop.png";
+import heroImage1_desktop_2x from "../../../../images/shared/desktop/hero-image-1-desktop@2x.png";
+import heroBgMobile from "../../../../images/shared/mobile/backgrounds/hero-bg-mobile.png";
+import heroBg2xMobile from "../../../../images/shared/mobile/backgrounds/hero-bg-mobile@2x.png";
+import heroBgDesktop from "../../../../images/shared/desktop/backgrounds/hero-bg-desktop.png";
+import heroBg2xDesktop from "../../../../images/shared/desktop/backgrounds/hero-bg-desktop@2x.png";
 
 const HeroSection = () => {
   return (
     <section
-      className={`section ${css.heroSection} overflow-hidden md:flex md:overflow-visible`}
+      className={`section relative ${css.heroSection} overflow-hidden md:flex md:bg-right-bottom md:overflow-visible xlg:px-20`}
     >
-      <div className={`container`}>
-        <div>
-          <h1 className={`lg:w-1/2 lg:flex lg:flex-col`}>
-            Maryna{" "}
-            <span className="relative flex flex-col items-start left-28 lg:left-32">
-              Holovan <p className="text-left">Ваша історія в кожній фотографії</p>
+      <div
+        className={`container md:flex xlg:justify-between xlg:pt-32 xlg:pb-20`}
+      >
+        <div className="md:w-1/2 xlg:flex xlg:flex-col">
+          <h1 className={`flex flex-col xlg:w-96 xlg:text-7xl`}>
+            Maryna
+            <span className="relative left-10 sm:left-24 mk:left-24 lg:left-32 xlg:text-right xlg:-left-3">
+              Holovan
             </span>
           </h1>
-          <div className="flex flex-row-reverse justify-between">
-            {/* <Icon id={"curve-arrow"} width={89} style={{}} /> */}
-            <svg
-              href={`${icons}#curve-arrow`}
-              stroke="currentColor"
-              fill="currentColor"
-              width={89}
-              height={150}
-            />
+
+          <p className="text-right relative xlg:text-center">
+            Ваша історія в кожній фотографії
+          </p>
+
+          <div className="flex flex-row justify-between xlg:justify-self-end">
             <button className="flex gap-2 items-center self-end">
               Записатися
               <Icon id={"arrow-white"} width={13} height={13} />
             </button>
+            <div>
+              <Icon
+                id={"curve-arrow"}
+                width={89}
+                style={{ position: "relative", right: "20px" }}
+              />
+            </div>
           </div>
+
         </div>
-        <div className={`${css.imageContainer} flex relative`}>
-          <img
-            src={heroImage1}
-            srcSet={`${heroImage1} 1x, ${heroImage1_2x} 2x`}
-            alt=""
-          />
-          <img
-            src={heroImage1}
-            srcSet={`${heroImage1} 1x, ${heroImage1_2x} 2x`}
-            className="relative top-2"
-            alt=""
-          />
-          {/* <img src={heroBg} className="absolute" alt="" /> */}
+
+        <div className={`${css.imageContainer} flex relative left-5`}>
+
+          <picture className="w-full">
+            <source
+              media="(min-width: 1440px)"
+              srcSet={`${heroImage1_desktop} 1x, ${heroImage1_desktop_2x} 2x`}
+            />
+            <img
+              src={heroImage1}
+              srcSet={`${heroImage1} 1x, ${heroImage1_2x} 2x`}
+              alt=""
+            />
+          </picture>
+
+          <picture className="w-full relative top-4">
+            <source
+              media="(min-width: 1440px)"
+              srcSet={`${heroImage1_desktop} 1x, ${heroImage1_desktop_2x} 2x`}
+            />
+
+            {/* <source
+              media="(min-width:320px)"
+              srcSet={`${heroImage1} 1x, ${heroImage1_2x} 2x`}
+            /> */}
+            <img
+              src={heroImage1}
+              srcSet={`${heroImage1} 1x, ${heroImage1_2x} 2x`}
+              alt=""
+            />
+          </picture>
+
+        </div>
+
+        <div className={`absolute -z-10 bottom-0 right-0 `}>
+          <picture className="w-full relative">
+            <source
+              media="(min-width: 1440px)"
+              srcSet={`${heroBgDesktop} 1x, ${heroBg2xDesktop} 2x`}
+            />
+            <img
+              src={heroImage1}
+              srcSet={`${heroBgMobile} 1x, ${heroBg2xMobile} 2x`}
+              alt=""
+            />
+          </picture>
         </div>
       </div>
     </section>
