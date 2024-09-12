86fa5589e02c718c19d4b0c3942e6d72dacad558
4svEXE <4sv.exe@gmail.com>
2024-09-12 17:04:28 +0200

    refactor/HomePage - PriceSection was refactored

--- src/Pages/HomePage/components/PriceSection/PriceSection.jsx
+++ src/Pages/HomePage/components/PriceSection/PriceSection.jsx
@@ -1,64 +1,60 @@
 import css from "./PriceSection.module.scss";
 
+const services = [
+  {
+    name: "Express",
+    price: "500 кс",
+    duration: "30 хв",
+    photos: "5 шт",
+    printPhotos: null,
+  },
+  {
+    name: "Standart",
+    price: "1500 кс",
+    duration: "60 хв",
+    photos: "15 шт",
+    printPhotos: "5 шт",
+  },
+  {
+    name: "Premium",
+    price: "2 500 кс",
+    duration: "90 хв",
+    photos: "25 шт",
+    printPhotos: "15 шт",
+  },
+];
+
 const PriceSection = () => {
   return (
     <section className={`section ${css.priceSection}`}>
       <div className="container">
         <h2 className="mb-16">Прайс</h2>
-        <ul className="flex flex-col gap-6 xlg:flex-row xlg:justify-between">
-          <li>
-            <div className={`${css.priceContainer} `}>
-              <h3>Express</h3>
+        {/* Варік з грідами замість флексів (лаконічніше)*/}
+        <ul className="grid gap-6 lg:grid-cols-3 grid-cols-1">
+          {/* лаконічніше виводити повторювані дані циклом аби дотримуватись принципу DRY */}
+          {services.map((service, index) => (
+
+            // забрав дів і дав клас одразу лішці аби зменшити код
+            <li key={index} className={`${css.priceContainer}`}>
+              <h3>{service.name}</h3>
               <ul>
                 <li>
-                  Ціна <span>500 кс</span>
+                  Ціна <span>{service.price}</span>
                 </li>
                 <li>
-                  Тривалість <span>30 хв</span>
+                  Тривалість <span>{service.duration}</span>
                 </li>
                 <li>
-                  Фото в обробці <span>5 шт</span>
+                  Фото в обробці <span>{service.photos}</span>
                 </li>
+                {service.printPhotos && (
+                  <li>
+                    Фото в друк <span>{service.printPhotos}</span>
+                  </li>
+                )}
               </ul>
-            </div>
-          </li>
-
-          <li>
-            <div className={`${css.priceContainer} `}>
-              <h3>Standart</h3>
-              <ul>
-                <li>
-                  Ціна <span>1500 кс</span>
-                </li>
-                <li>
-                  Тривалість <span>60 хв</span>
-                </li>
-                <li>
-                  Фото в обробці <span>15 шт</span>
-                </li>
-                <li>Фото в друк <span>5 шт</span></li>
-              </ul>
-            </div>
-          </li>
-
-          <li>
-          <div className={`${css.priceContainer} `}>
-              <h3>Premium</h3>
-              <ul>
-                <li>
-                  Ціна <span>2 500 кс</span>
-                </li>
-                <li>
-                  Тривалість <span>90 хв</span>
-                </li>
-                <li>
-                  Фото в обробці <span>25 шт</span>
-                </li>
-                <li>Фото в друк <span>15 шт</span></li>
-              </ul>
-            </div>
-
-          </li>
+            </li>
+          ))}
         </ul>
       </div>
     </section>
