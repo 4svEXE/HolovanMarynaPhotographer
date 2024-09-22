19de51c4a9b9e7424e429e7b09e3df1a2b7d8820
klimisnotavailable <klimgrigor28@gmail.com>
2024-09-22 20:06:58 +0300

    klym/reviews/feat
    add review section

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -2,6 +2,7 @@ import HeroSection from "./components/HeroSection/HeroSection";
 import IntroductionSection from "./components/IntroductionSection/IntroductionSection";
 import GallerySection from "./components/GallerySection/GallerySection";
 import PriceSection from "./components/PriceSection/PriceSection";
+import ReviewSection from "./components/ReviewSection/ReviewSection";
 
 const HomePage = () => {
 return (
@@ -10,6 +11,7 @@ return (
         <IntroductionSection></IntroductionSection>
         <GallerySection></GallerySection>
         <PriceSection></PriceSection>
+        <ReviewSection></ReviewSection>
     </>
 );
 };
