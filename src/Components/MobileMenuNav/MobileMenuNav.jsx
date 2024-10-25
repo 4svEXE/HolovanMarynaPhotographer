f8fa255be4ce89f10fbacae0d6a7b285af1a06e7
klimisnotavailable <klimgrigor28@gmail.com>
2024-10-25 17:56:57 +0300

    klym/mobileMenu/feat
    add navigation links to mobile menu

--- src/Components/MobileMenuNav/MobileMenuNav.jsx
+++ src/Components/MobileMenuNav/MobileMenuNav.jsx
@@ -1,12 +1,23 @@
 import css from "./MobileMenuNav.module.scss";
+import { useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const MobileMenuNav = () => {
+  const location = useLocation();
+
   return (
-    <nav className={""}>
+    <nav>
       <ul className={`${css.navList} mb-40`}>
-        <li className={css.bold}><a href="">Про мене</a></li>
-        <li><a href="">Портфоліо</a></li>
-        <li><a href="">Контакти</a></li>
+      <li className={`${location.pathname.includes("home") ? css.bold : ""}`}>
+          <Link className={css.checked} to={"HolovanMarynaPhotographer/home"}>
+          Про мене</Link>
+        </li>
+        <li className={`${location.pathname.includes("portfolio") ? css.bold : ""}`}>
+          <Link to={"HolovanMarynaPhotographer/portfolio"}>Портфоліо</Link>
+        </li>
+        <li className={`${location.pathname.includes("contacts") ? css.bold : ""}`}>
+          <Link to={"HolovanMarynaPhotographer/contacts"}>Контакти</Link>
+        </li>
       </ul>
     </nav>
   );
