d6e64f8db0a27a73dc444749ddba0be408633226
klimisnotavailable <klimgrigor28@gmail.com>
2024-11-21 01:16:19 +0200

    klym/localization/feat
    add locales, FAQ localization

--- src/Pages/HomePage/components/FAQ/FAQ.jsx
+++ src/Pages/HomePage/components/FAQ/FAQ.jsx
@@ -2,19 +2,25 @@ import Accordion from "accordion-js";
 import "accordion-js/dist/accordion.min.css";
 import { useEffect } from "react";
 import css from "./FAQ.module.scss";
-import questions from "./questionData";
+import data from "../../../../localization/locale/ua.json";
+import { useTranslation } from "react-i18next";
 
 const FAQ = () => {
   useEffect(() => {
     new Accordion(".accordion-container");
   },[]);
 
+  const {t} = useTranslation();
+
+  const questionData = data.homepage.FAQ;
+
   return (
     <section className={`${css.FAQ} pt-20 pb-20 xlg:pt-28 xlg:pb-28`}>
       <div className={`container ${css.FAQContainer}`}>
         <h2 className="text-center mb-10">FAQ</h2>
         <ul className={`accordion-container`}>
-          {questions.map((item, index) => {
+          {questionData.map((item, index) => {
+            console.log(item)
             return (
               <li
                 key={index}
@@ -23,7 +29,7 @@ const FAQ = () => {
                 <div>
                   <h3 className={`ac-header`}>
                     <button type="button" className={`ac-trigger`}>
-                      {item.question}
+                      {t(`homepage.FAQ.${index}.question`)}
                     </button>
                   </h3>
                   <p className="acItemIcon absolute right-2 top-9 md:top-5 xlg:top-14 xlg:right-4">
@@ -31,7 +37,7 @@ const FAQ = () => {
                   </p>
                 </div>
                 <div className={`ac-panel`}>
-                  <p className={`ac-text`}>{item.answer}</p>
+                  <p className={`ac-text`}>{t(`homepage.FAQ.${index}.answer`)}</p>
                 </div>
               </li>
             );
