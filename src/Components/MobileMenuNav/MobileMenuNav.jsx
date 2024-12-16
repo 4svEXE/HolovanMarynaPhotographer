a83b63ee2dc98e6fb15ab046bd6ff672b0e7ca49
klimisnotavailable <klimgrigor28@gmail.com>
2024-12-16 14:25:59 +0200

    klym/mobileMenuNav/feat

--- src/Components/MobileMenuNav/MobileMenuNav.jsx
+++ src/Components/MobileMenuNav/MobileMenuNav.jsx
@@ -1,8 +1,10 @@
 import css from "./MobileMenuNav.module.scss";
 import { useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 const MobileMenuNav = () => {
+  const {t} = useTranslation();
   const location = useLocation();
 
   return (
@@ -10,13 +12,13 @@ const MobileMenuNav = () => {
       <ul className={`${css.navList} mb-40`}>
       <li className={`${location.pathname.includes("home") ? css.bold : ""}`}>
           <Link className={css.checked} to={"HolovanMarynaPhotographer/home"}>
-          Про мене</Link>
+          {t('header.nav.AboutMe')}</Link>
         </li>
         <li className={`${location.pathname.includes("portfolio") ? css.bold : ""}`}>
-          <Link to={"HolovanMarynaPhotographer/portfolio"}>Портфоліо</Link>
+          <Link to={"HolovanMarynaPhotographer/portfolio"}>{t('header.nav.Portfolio')}</Link>
         </li>
         <li className={`${location.pathname.includes("contacts") ? css.bold : ""}`}>
-          <Link to={"HolovanMarynaPhotographer/contacts"}>Контакти</Link>
+          <Link to={"HolovanMarynaPhotographer/contacts"}>{t('header.nav.Contacts')}</Link>
         </li>
       </ul>
     </nav>
