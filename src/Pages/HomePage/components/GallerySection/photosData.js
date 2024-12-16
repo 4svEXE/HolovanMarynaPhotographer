bf6a0aa2359738e9a780e2f653942990a1a9ab5d
klimisnotavailable <klimgrigor28@gmail.com>
2024-12-16 15:30:22 +0200

    klym/GallerySection/Feat
    optimized gallery list logic and add category filter

---
+++ src/Pages/HomePage/components/GallerySection/photosData.js
@@ -0,0 +1,77 @@
+import galleryImage1 from "../../../../images/shared/mobile/portfolio/Gallery-picture-1.png";
+import galleryImage1_2x from "../../../../images/shared/mobile/portfolio/Gallery-picture-1@2x.png";
+import galleryImage2 from "../../../../images/shared/mobile/portfolio/Gallery-picture-2.png";
+import galleryImage2_2x from "../../../../images/shared/mobile/portfolio/Gallery-picture-2@2x.png";
+import galleryImage3 from "../../../../images/shared/mobile/portfolio/Gallery-picture-3.png";
+import galleryImage3_2x from "../../../../images/shared/mobile/portfolio/Gallery-picture-3@2x.png";
+import galleryImage4 from "../../../../images/shared/mobile/portfolio/Gallery-picture-4.png";
+import galleryImage4_2x from "../../../../images/shared/mobile/portfolio/Gallery-picture-4@2x.png";
+import galleryImage5 from "../../../../images/shared/mobile/portfolio/Gallery-picture-5.png";
+import galleryImage5_2x from "../../../../images/shared/mobile/portfolio/Gallery-picture-5@2x.png";
+import galleryImage6 from "../../../../images/shared/mobile/portfolio/Gallery-picture-6.png";
+import galleryImage6_2x from "../../../../images/shared/mobile/portfolio/Gallery-picture-6@2x.png";
+
+import galleryImage1Desktop from "../../../../images/shared/desktop/portfolio/Gallery-picture-1.png";
+import galleryImage1_2xDesktop from "../../../../images/shared/desktop/portfolio/Gallery-picture-1@2x.png";
+import galleryImage2Desktop from "../../../../images/shared/desktop/portfolio/Gallery-picture-2.png";
+import galleryImage2_2xDesktop from "../../../../images/shared/desktop/portfolio/Gallery-picture-2@2x.png";
+import galleryImage3Desktop from "../../../../images/shared/desktop/portfolio/Gallery-picture-3.png";
+import galleryImage3_2xDesktop from "../../../../images/shared/desktop/portfolio/Gallery-picture-3@2x.png";
+import galleryImage4Desktop from "../../../../images/shared/desktop/portfolio/Gallery-picture-4.png";
+import galleryImage4_2xDesktop from "../../../../images/shared/desktop/portfolio/Gallery-picture-4@2x.png";
+import galleryImage5Desktop from "../../../../images/shared/desktop/portfolio/Gallery-picture-5.png";
+import galleryImage5_2xDesktop from "../../../../images/shared/desktop/portfolio/Gallery-picture-5@2x.png";
+import galleryImage6Desktop from "../../../../images/shared/desktop/portfolio/Gallery-picture-6.png";
+import galleryImage6_2xDesktop from "../../../../images/shared/desktop/portfolio/Gallery-picture-6@2x.png";
+
+const photosList = [
+  {
+    mobile: galleryImage1,
+    mobile2x: galleryImage1_2x,
+    desktop: galleryImage1Desktop,
+    desktop2x: galleryImage1_2xDesktop,
+    category:"Portrait"
+  },
+  {
+    mobile: galleryImage2,
+    mobile2x: galleryImage2_2x,
+    desktop: galleryImage2Desktop,
+    desktop2x: galleryImage2_2xDesktop,
+    category:"Portrait"
+
+  },
+  {
+    mobile: galleryImage3,
+    mobile2x: galleryImage3_2x,
+    desktop: galleryImage3Desktop,
+    desktop2x: galleryImage3_2xDesktop,
+    category:"Love Story"
+
+  },
+  {
+    mobile: galleryImage4,
+    mobile2x: galleryImage4_2x,
+    desktop: galleryImage4Desktop,
+    desktop2x: galleryImage4_2xDesktop,
+    category:"Love Story"
+
+  },
+  {
+    mobile: galleryImage5,
+    mobile2x: galleryImage5_2x,
+    desktop: galleryImage5Desktop,
+    desktop2x: galleryImage5_2xDesktop,
+    category:"Portrait"
+
+  },
+  {
+    mobile: galleryImage6,
+    mobile2x: galleryImage6_2x,
+    desktop: galleryImage6Desktop,
+    desktop2x: galleryImage6_2xDesktop,
+    category:"Portrait"
+
+  },
+];
+
+export default photosList;
